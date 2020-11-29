0c107252f3f1184edbd3d6c8b5dc3b152680aa86
Esteban.franco <estebanfrancobedoya@gmail.com>
2020-11-29 05:32:17 -0500

    reparing first class folder

---
+++ 01.first_script/helloW2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Diferencias entre las comillas y los espacios
+
+echo "Hello 	world"
+echo "hello world"
+echo hello * World
+echo hello 	world
+echo "hello"*"World"
+echo 'hello' world
+echo `hello` world
